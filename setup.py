33d9c6343dc17095eab2f53f9f6719af33f2f816
Jason Kraus <jasonk@cukerinteractive.com>
2012-05-11 21:42:22 -0700

    updated test loader, setup.py test now works

--- setup.py
+++ setup.py
@@ -1,2 +1,37 @@
-# dummy setup.py for tox
-from distutils.core import setup; setup()
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
+
+VERSION = '0.0.1'
+LONG_DESC = """\
+"""
+
+setup(name='django-ci',
+      version=VERSION,
+      description="",
+      long_description=LONG_DESC,
+      classifiers=[
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+      ],
+      keywords='django ci testing',
+      #maintainer = 'Jason Kraus',
+      #maintainer_email = ',
+      url='https://github.com/jonashaag/django-ci',
+      #license='New BSD License',
+      packages=find_packages(exclude=['test_environment', 'tests']),
+      test_suite='tests.setuptest.SetupTestSuite',
+      tests_require=(
+        'pep8',
+        'coverage',
+        'django',
+      ),
+      include_package_data = True,
+  )
